ee8c1fd08b92d503ede3b23e50ad8d877f2d794d
Stéphano TELOLAHY <stephanotelolahy@gmail.com>
2017-02-03 16:23:42 +0100

    initial commit

---
+++ app/src/main/java/com/creativegames/belote/MainActivity.java
@@ -0,0 +1,12 @@
+package com.creativegames.belote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
